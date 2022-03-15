32ab67003a48eebc9031bee4fc0451b09ccbcebe
SindileM <smakhathini24@gmail.com>
2022-03-15 16:32:10 +0200

    set up

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="https://i.postimg.cc/14xhL4hV/btslanding.jpg" alt="" srcset="">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    
+  }
+}
+</script>
